05754727b4d44fa189af506a5d331d38adf17bd1
Paul <pprommer@salesforce.com>
2017-01-25 21:36:59 -0700

    Fixing tests and adding another one

--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -5,5 +5,9 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('adds 1 + 1 to equal 2', () => {
-  expect(sum(1, 1)).toBe(3);
+  expect(sum(1, 1)).toBe(2);
+});
+
+test('adds 1 + 0 to equal 1', () => {
+  expect(sum(1, 0)).toBe(1);
 });
